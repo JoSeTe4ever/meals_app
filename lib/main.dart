ca1c3a5f6ea319351bcb4a83883032b202ccc53e
josete <jose-herminio.parreno@basf.com>
2024-08-28 09:47:31 +0200

    feat: add go_route dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/models/favourite_meals.dart';
 import 'package:meals_app/screens/categories.dart';
+import 'package:provider/provider.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -13,7 +15,11 @@ final theme = ThemeData(
 );
 
 void main() {
-  runApp(const App());
+  runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => FavouriteMeals()),
+      ],
+      child: const App()));
 }
 
 class App extends StatelessWidget {
@@ -22,8 +28,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: theme,
-        home: const CategoriesScreen(),
+      theme: theme,
+      home: const CategoriesScreen(),
     );
   }
 }
---
+++ lib/models/favourite_meals.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'meal.dart';
+
+class FavouriteMeals extends ChangeNotifier {
+  final List<Meal> _favouriteMeals = [];
+
+  void addFavouriteMeal(Meal meal) {
+    _favouriteMeals.add(meal);
+    notifyListeners();
+  }
+
+  void removeFavouriteMeal(Meal meal) {
+    _favouriteMeals.remove(meal);
+    notifyListeners();
+  }
+
+
+}
